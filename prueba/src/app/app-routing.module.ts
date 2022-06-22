cc52eac27bfa923e63d8058f46b60c4e5ea46b50
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-22 15:39:56 -0500

    finishing home compiknent

--- prueba/src/app/app-routing.module.ts
+++ prueba/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'company', loadChildren: () => import('./pages/company/company/company.module').then(m => m.CompanyModule) }, { path: 'empanada', loadChildren: () => import('./pages/company/empanada/empanada.module').then(m => m.EmpanadaModule) }];
+const routes: Routes = [{ path: 'company', loadChildren: () => import('./pages/company/company/company.module').then(m => m.CompanyModule) }, { path: 'empanada', loadChildren: () => import('./pages/company/empanada/empanada.module').then(m => m.EmpanadaModule) }, { path: 'home', loadChildren: () => import('./pages/company/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
