883c26a6beab060f5db5162bbeaedd847e9143a0
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-06-22 12:18:10 -0500

    ng g m c  shared/components/contanct --module app

--- prueba/src/app/app.module.ts
+++ prueba/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { HeaderModule } from './shared/components/header/header.module';
+import { ContanctComponent } from './shared/components/contanct/contanct.component';
+import { ContanctModule } from './shared/components/contanct/contanct.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContanctComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeaderModule
+    HeaderModule,
+    ContanctModule
   ],
   providers: [],
   bootstrap: [AppComponent]
